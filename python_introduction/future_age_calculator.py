2efe99e424e45a874e3dc85ef0f0339019a340bd
awanat-olawale <olawaleawanat05@gmail.com>
2025-05-20 02:02:20 +0100

    Task 4 receives user input to perform basic arithmetic

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,4 +1,11 @@
+#Accepting user input
+
 user_age = int(input("How old are you? "))
+
+#Estimating new age of the user
+
 new_age = 27 + user_age
+
+#Printing results
 
 print(f"In 2050, you will be {new_age} years old.")
