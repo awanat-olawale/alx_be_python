e99bc348cda7740982e2ba521eb467f0dc72b352
awanat-olawale <olawaleawanat05@gmail.com>
2025-05-20 02:06:34 +0100

    Task 2 calculates the area of a rectangle with the usage of predefined variables

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -1,6 +1,12 @@
+#Predefined values for length and width of a rectangle
+
 length = 10
 width = 5
 
+#Calculating the area of a rectangle
+
 area = length * width
+
+#Printing results
 
 print(f"The area of the rectangle is: {area}")
