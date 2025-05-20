3406528f8b52fcf61dcbdc8a38a9dc0fdf6f5f9b
awanat-olawale <olawaleawanat05@gmail.com>
2025-05-20 02:08:32 +0100

    Task 1 calculates simple interest through basic arithmetic

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,13 @@
+#Predefined values for calculating simple interest
+
 principal = 1000
 rate = 0.05
 time = 3
+
+#Estimating simple interest
+
 interest = principal * rate * time
+
+#Printing results
 
 print(f"The simple interest is: {interest}")
