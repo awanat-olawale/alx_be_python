0777a07a47a9196afdbf50f01511d71384c7100b
awanat-olawale <olawaleawanat05@gmail.com>
2025-05-20 02:00:01 +0100

    Task 5 handles arithmetic operations without using conditional statements

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,17 @@
+#Accepting user input
+
 monthly_income = input("Enter your monthly income: ")
 monthly_expenses = input("Enter your total monthly expenses: ")
 
+#Calculating monthly savings
+
 monthly_savings = monthly_income - monthly_expenses
 
+#Estimating projected annual savings
+
 projected_annual_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+
+#Printing the results
 
 print(f"Your monthly savings are ${monthly_savings}")
 print(f"Projected savings after one year, with interest, is: ${int(projected_annual_savings)}.")
