78dfc40f3abf7a182501a7ca132ac905ec5cc418
awanat-olawale <olawaleawanat05@gmail.com>
2025-05-20 02:11:36 +0100

    Task 0 helps practice basic arithmetic operations through predefined calculations

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,15 @@
+#Predefined values for performing basic operations
+
 number1 = 10
 number2 = 5
+
+#Formulae for basic arithmetic operations
+
 addition = number1 + number2
 subtraction = number1 - number2
 multiplication = number1 * number2
+
+#Printing results
 
 print(f"Addition of {number1} and {number2} is {addition}")
 print(f"Subtraction of {number1} and {number2} is {subtraction}")
