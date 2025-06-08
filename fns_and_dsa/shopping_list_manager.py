0309c92c4f0fab383c0bf8d68774491631f255e4
awanat-olawale <olawaleawanat05@gmail.com>
2025-06-08 23:12:09 +0100

    Task 1 implements a simple interface for managing a shopping list

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,34 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            item = input("Enter item of choice: ")
+            shopping_list.append(item)
+            print(f"{item} has been added to the list")
+        elif choice == '2':
+            item = input("Enter item of choice: ")
+            if item in shopping_list:
+                shopping_list.remove(item)
+                print(f"{item} has been removed from the list")
+            else:
+                print(f"{item} is not found in list")
+        elif choice == '3':
+            print(shopping_list)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
